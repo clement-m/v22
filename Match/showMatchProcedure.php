b986d5c6274fdcae744edd13abfd9b2346d816f8
Clement Melchior <clement.melchior@gmail.com>
2016-06-11 10:34:52 +0200

    remake process in 3 part with bdd event with table match

--- Match/showMatchProcedure.php
+++ Match/showMatchProcedure.php
@@ -8,18 +8,20 @@ $templateParams['mastery_Level'] = $_POST['ml'];
 
 include('../Match/showMatchFunctions.php');
 
-updatePlayer($_POST['pi'],$_POST['pn']);
-updateGod($_POST['gi'],$_POST['gn']);
 
-$result = array();
-$rank = getRank($_POST['pi'],$_POST['gi']);
+
+//updatePlayer($_POST['pi'],$_POST['pn']);
+//updateGod($_POST['gi'],$_POST['gn']);
+
+//$result = array();
+//$rank = getRank($_POST['pi'],$_POST['gi']);
 //$kda = getKda($_POST['pi'],$_POST['gi']);
 //$league = getLeague($_POST['pi']);
 
-if(!$rank) {
-  $templateParams['rank'] = getAPIRank($_POST['pi'], $_POST['s'], $_POST['gi']);
-  $templateParams['kda'] = getAPIKda($_POST['pi'],$_POST['q'],$_POST['s']);
-}
-else echo "else";
+//if(!$rank) {
+  //$templateParams['rank'] = getAPIRank($_POST['pi'], $_POST['s'], $_POST['gi']);
+  //$templateParams['kda'] = getAPIKda($_POST['pi'],$_POST['q'],$_POST['s']);
+//}
+//else echo "else";
 
 showMatch($templateParams);
