aece566719cc5abfe3b4982ca54cd8bf3652a04a
Clement Melchior <clement.melchior@gmail.com>
2016-06-14 18:06:18 +0200

    prepare test ovh+git remote n push 1/3

---
+++ LIB/smLib/co sm.php
@@ -0,0 +1,32 @@
+<?php
+
+try
+{
+  $db_config = array();
+  $db_config['SGBD']  = 'mysql';
+  $db_config['HOST']  = 'smitepansnspazki.mysql.db';
+  $db_config['DB_NAME'] = 'smitepansnspazki';
+  $db_config['USER']  = 'smitepansnspazki';
+  $db_config['PASSWORD']  = 'mlkqsdSmite6';
+  $db_config['OPTIONS'] = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  );
+  
+  $this->pdo = new PDO(
+    $db_config['SGBD'] .':host='. $db_config['HOST'] .';dbname='. $db_config['DB_NAME'],
+  $db_config['USER'],
+  $db_config['PASSWORD'],
+  $db_config['OPTIONS']);
+
+  unset($db_config);
+
+  $this->match->connection($this->pdo);
+  $this->player->connection($this->pdo);
+  $this->rank->connection($this->pdo);
+  $this->kda->connection($this->pdo);
+}
+catch(PDOException $e)
+{
+  die('Erreur : ' . $e->getMessage());
+}
