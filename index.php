42f58989e287c4c810b38bfc2e6fd5a8d59a0543
Clement Melchior <clement.melchior@gmail.com>
2016-06-15 18:25:07 +0200

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'LIB/twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 //$twig = new Twig_Environment($loader, array('cache' => 'cache'));
-$loader= new Twig_Loader_Filesystem('src/Views');
+$loader= new Twig_Loader_Filesystem('SRC/Views');
 $twig= new Twig_Environment($loader);
 $template= $twig->loadTemplate('index.html.twig');
 echo $template->render(array());
