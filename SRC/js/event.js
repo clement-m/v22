c963c44a13a6c3e32aeaa148402ee535acf99f9f
Clement Melchior <clement.melchior@gmail.com>
2016-06-19 03:17:27 +0200

    v0.9.9.3 - correction event matchChange

--- SRC/js/event.js
+++ SRC/js/event.js
@@ -1,5 +1,7 @@
 function start(e) {
     e.preventDefault();
+    $('#team1').bind("DOMSubtreeModified", function() { changeTeamEvent(1); });
+    $('#team2').bind("DOMSubtreeModified", function() { changeTeamEvent(2); });
     getConnection();
 }
 
@@ -7,7 +9,5 @@ $(document).ready(function() {
     $('#startInput').focus();
     $('#startInput').keypress(function (e) { if (e.which == 13) start(e); });
     $('#startBtn').click(function(e) { start(e); });
-    $('#team1').bind("DOMSubtreeModified", function() { changeTeamEvent(1); });
-    $('#team2').bind("DOMSubtreeModified", function() { changeTeamEvent(2); });
 });
 
