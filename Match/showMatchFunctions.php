2e729162b68b03fc718c0d98f6bb3934ae469505
Clement Melchior <clement.melchior@gmail.com>
2016-06-22 11:18:40 +0200

    no cache on local version

--- Match/showMatchFunctions.php
+++ Match/showMatchFunctions.php
@@ -53,7 +53,7 @@ function quickMatch($data){
   require_once '../LIB/twig/lib/Twig/Autoloader.php';
   Twig_Autoloader::register();
   $loader = new Twig_Loader_Filesystem('../SRC/Views');
-  $twig = new Twig_Environment($loader, array('debug' => true, 'cache' => 'cache'));
+  $twig = new Twig_Environment($loader);
   $twig->addExtension(new Twig_Extension_Debug());
   $template = $twig->loadTemplate('quickPlayer.html.twig');
   echo $template->render(array('data' => $data));
